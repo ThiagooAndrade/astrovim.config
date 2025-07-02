86463f00539c5ad92aaf9120b2f89f6c87ad322d
ThiagooAndrade <andradethiago1609@gmail.com>
2025-07-01 21:17:17 -0300

    add: vesper theme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,8 +1,23 @@
 return {
   {
-    "vinitkumar/oscura-vim",
-    lazy = false,
+    "datsfilipe/vesper.nvim",
+    lazy = true,
+    config = function()
+      require("vesper").setup {
+        transparent = true, -- Boolean: Sets the background to transparent
+        italics = {
+          comments = true, -- Boolean: Italicizes comments
+          keywords = false, -- Boolean: Italicizes keywords
+          functions = false, -- Boolean: Italicizes functions
+          strings = true, -- Boolean: Italicizes strings
+          variables = false, -- Boolean: Italicizes variables
+        },
+        overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
+        palette_overrides = {},
+      }
+    end,
   },
+  { "vinitkumar/oscura-vim", lazy = true },
   { "folke/tokyonight.nvim", lazy = true },
   { "sainnhe/everforest", lazy = true },
   { "craftzdog/solarized-osaka.nvim", lazy = true, opts = {
@@ -27,21 +42,6 @@ return {
   },
   {
     "datsfilipe/min-theme.nvim",
-    lazy = true,
-    config = function()
-      require("min-theme").setup {
-        -- (note: if your configuration sets vim.o.background the following option will do nothing!)
-        theme = "dark", -- String: 'dark' or 'light', determines the colorscheme used
-        transparent = true, -- Boolean: Sets the background to transparent
-        italics = {
-          comments = false, -- Boolean: Italicizes comments
-          keywords = false, -- Boolean: Italicizes keywords
-          functions = false, -- Boolean: Italicizes functions
-          strings = false, -- Boolean: Italicizes strings
-          variables = false, -- Boolean: Italicizes variables
-        },
-        overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
-      }
-    end,
+    lazy = true
   },
 }
